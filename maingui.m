7b4c02a0970578bf2f567cce3048acdd5bb69426
Chae Jubb <cjubb39@gmail.com>
2014-03-01 20:01:56 -0500

    Import targets from XML and cycle through them

--- maingui.m
+++ maingui.m
@@ -61,6 +61,17 @@ guidata(hObject, handles);
 % UIWAIT makes maingui wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
+global targets_array;
+targets = repmat(struct( ...
+    'identifier', '', ...
+    'name', '', ...
+    'Notes', '', ...
+    'Latitude', 0, ...
+    'Longitude', 0, ...
+    'Time', clock ...
+ ), 1024, 1);
+targets_array = struct('targets', targets, 'current', 0, 'count', 0);
+
 %% Set up live update
 t = timer('ExecutionMode', 'fixedRate', 'Period', 2);
 t.TimerFcn = {@updatePosition, handles};
@@ -87,6 +98,17 @@ if (get(hObject, 'Value') == get(hObject, 'Max'))
     end
 end
 
+function addToTargets(incoming, handles)
+global targets_array;
+targets_array.target(...
+    targets_array.count + 1) ...
+    = ...
+    incoming;
+targets_array.count = targets_array.count + 1;
+if (targets_array.current == 0)
+    targets_array.current = 1;
+    updateTarget(handles);
+end
 
 
 % --- Outputs from this function are returned to the command line.
@@ -101,10 +123,17 @@ varargout{1} = handles.output;
 
 
 % --- Executes on button press in nextTarget.
-function nextTarget_Callback(hObject, eventdata, handles)
+function nextTarget_Callback(~, ~, handles)
 % hObject    handle to nextTarget (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+global targets_array;
+targets_array.current = targets_array.current + 1;
+if (targets_array.current > targets_array.count)
+    targets_array.current = 1;
+end
+
+updateTarget(handles);
 
 
 % --- Executes on button press in addtarget.
@@ -115,17 +144,57 @@ function addtarget_Callback(hObject, eventdata, handles)
 
 
 % --- Executes on button press in importtargets.
-function importtargets_Callback(hObject, eventdata, handles)
+function importtargets_Callback(~, ~, handles)
 % hObject    handle to importtargets (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+[FileName,PathName] = uigetfile('*.xml', 'Select XML of Targets', 'targets.xml');
+fullXMLpath = strcat(PathName, FileName);
+
+%xml_targets = xmlread(fullXMLpath);
+targets_struct = xml2struct(fullXMLpath);
+sites_root = targets_struct.wmc__TEOSiteMgr.EOSites.wmc__TEOSite;
+
+for i = 1:length(sites_root);
+    handleXMLTarget(sites_root{i}, handles);
+end
+
+function handleXMLTarget(target, handles)
+positionPattern = 'Closest approach lat: (-?\d*.?\d*), lon: (-?\d*.?\d*) at (\d{2}):(\d{2}):(\d{2})';
+[match, token] = regexp(target.Attributes.Notes, positionPattern, 'match', 'tokens');
+
+if (isempty(match))
+    token = cell(1);
+    token{1}(1:5) = '0';
+end
+
+curDate = clock;
+addToTargets(struct( ...
+    'identifier', target.Attributes.Ident, ...
+    'name', target.Attributes.Nomenclature, ...
+    'Notes', target.Attributes.Notes, ...
+    'Latitude', token{1}(1), ...
+    'Longitude', token{1}(2), ...
+    'Time', datenum(curDate(1), curDate(2), curDate(3), str2double(token{1}(3)), ...
+        str2double(token{1}(4)), str2double(token{1}(5))) ...
+ ), handles);
+
+function updateTarget(handles)
+global targets_array;
+newTarget = targets_array.target(targets_array.current);
+set(handles.targetname, 'String', newTarget.name);
+set(handles.targetcoord, 'String', strcat({'Lat: '}, newTarget.Latitude, ...
+    {'; Lon: '}, newTarget.Longitude));
+set(handles.targetCountdown, 'String', datestr(newTarget.Time, 'HH:MM:SS'));
+set(handles.targetNotes, 'String', newTarget.Notes);
+
 
 % --- Executes on button press in liveposupdate.
-function liveposupdate_Callback(hObject, eventdata, handles)
+function liveposupdate_Callback(~, ~, handles)
 % hObject    handle to liveposupdate (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of liveposupdate
-
+updatePosition(0,0,handles);
